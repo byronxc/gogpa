17aaee7b3f7cde52ea55f0fccaaf955a37259515
byronxc <bapineda22@gmail.com>
2020-01-13 15:18:51 -0800

    it displays the gpa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,14 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  credits:Array<number> = [];
+  mypoints:Array<number> = [];
+  favorite
+  total_points;
+  total_units;
+  index;
+  units = 0;
+  points = 0.00;
   items = [{'class' : 'ion-padding'}];
-  constructor() {
 
-  }
+  constructor(public toastController: ToastController) {}
+
   addNewCourse() {
     this.items.push({'class' : 'ion-padding'});
-}
-  
-}
+  }
 
+  calculateGPA() {
+    if(this.total_points && this.total_units){
+      this.points =+ this.total_points;
+      this.units =+ this.total_units;
+    }
 
+    for(var i = 0;i< this.credits.length; i++) {
+      if(this.credits[i] && this.mypoints[i]){
+        this.units += this.credits[i];
+        this.points += Number(this.mypoints[i]) * this.credits[i];
+      }  
+    } 
+  }
+
+  async displayGPA() {
+    this.calculateGPA()
+    
+    if(this.points && this.units){
+    const toast = await this.toastController.create({
+      message: "GPA: " + (Math.round(this.points)/this.units).toFixed(2),
+      duration: 6000
+    });
+    toast.present()
+    }
+    else{
+      const toast = await this.toastController.create({
+        message: "Please fill out all required fields",
+        duration: 6000
+      });
+      toast.present()
+    }
+    
+    this.units = 0;
+    this.points = 0.0;
+  }
+}
